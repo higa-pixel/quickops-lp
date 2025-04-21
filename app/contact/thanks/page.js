d2cfb761b0020a59841057aedf00e40693392517
higa-pixel <higatoshi0215@gmail.com>
2025-04-21 16:33:32 +0900

    修正: サンクスページのaタグ構文エラーを修正

--- app/contact/thanks/page.js
+++ app/contact/thanks/page.js
@@ -18,12 +18,14 @@ export default function Thanks() {
             QuickOpsについてさらに詳しく知りたい方は、以下の資料をダウンロードしてください。
           </p>
           
-            href="/downloads/quickops-service-document.pdf"
-            download
-            className="inline-block px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md"
-          >
-            資料をダウンロード
-          </a>
+        <a
+          href="/downloads/quickops-service-document.pdf"
+          download
+          className="inline-block px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md"
+        >
+          資料をダウンロード
+        </a>
+
         </div>
         
         <Link href="/" className="text-blue-600 hover:underline">
